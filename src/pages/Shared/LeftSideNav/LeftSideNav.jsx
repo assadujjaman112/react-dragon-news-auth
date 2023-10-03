339a04d028aa08e8a1358912a0dfcbf5668257bd
assadujjaman112 <assadujjamanmurad120@gmail.com>
2023-10-04 01:06:24 +0600

    added login

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,24 +1,25 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+
 const LeftSideNav = () => {
     const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        fetch('categories.json')
-            .then(res => res.json())
-            .then(data => setCategories(data))
-    }, [])
-
+    useEffect(()=>{
+        fetch("categories.json")
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    },[])
     return (
-        <div className="space-y-6">
-            <h2 className="text-2xl">All Categories</h2>
+        <div className="space-y-5">
+            <h2 className="text-3xl ">All Categories {categories.length} </h2>
+            <button className="btn w-11/12 mx-auto ml-3 bg-[#E7E7E7]">National News</button>
             {
-                categories.map(category => <Link 
-                    className="block ml-4 text-xl font-semibold" 
-                    key={category.id}
-                    to={`/category/${category.id}`}
-                    >{category.name}</Link>)
+                categories.map(category => <Link
+                     key={category.id}
+                    className="block ml-6 text-xl font-semibold"
+                     >{category.name}
+                     </Link>)
             }
         </div>
     );
